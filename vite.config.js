de80fc7e268937dd5205c72b0ca669c75e4e50f3
airinrs <airinrashidi@gmail.com>
2025-02-19 00:53:54 +0800

    first commit

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), sentryVitePlugin({
+    base: "/Apple-website",
     org: "jsm-eu",
     project: "javascript-react"
   })],
